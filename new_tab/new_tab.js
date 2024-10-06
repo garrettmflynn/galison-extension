79ed07f215b9bbba874ad64bde4960efd90d303f
Garrett Michael Flynn <garrettmflynn@gmail.com>
2024-10-06 11:36:37 -0700

    Load updates in the background

--- new_tab/new_tab.js
+++ new_tab/new_tab.js
@@ -12,16 +12,24 @@ const getRequestURL = (googleSheetUrl) => {
     return `https://docs.google.com/spreadsheets/d/${key}/gviz/tq?tqx=out:csv&sheet=${sheetName}`
 }
 
-const url = getRequestURL('https://docs.google.com/spreadsheets/d/1sxCPH3q_MdYJjDmeg61igLR5sPpo3WzDuY0O6Ra-i-M/edit?gid=0#gid=0')
+const getState = (name) => {
+    const state = localStorage.getItem(name)
+    if (state) return JSON.parse(state)
+    return null
+}
 
-const STATES = {}
+const setState = (name, state) => {
+    localStorage.setItem(name, JSON.stringify(state))
+}
+
+
+const url = getRequestURL('https://docs.google.com/spreadsheets/d/1sxCPH3q_MdYJjDmeg61igLR5sPpo3WzDuY0O6Ra-i-M/edit?gid=0#gid=0')
 
 const removeQuotes = (str) => str.slice(1, -1)
 const processCellValue = (value) => removeQuotes(value).trim()
 
-const getCSVData = async (url) => {
-    if (STATES[url]) return STATES[url]
-    const text = await fetch(url).then(response => response.text())
+const parseSheet = (text) => {
+
     const headers = text.split('\n')[0].split(',').map(processCellValue)
 
     const data = text.split('\n').slice(1).map(row => {
@@ -32,12 +40,20 @@ const getCSVData = async (url) => {
         }, {})
     })
 
-    const state = STATES[url] = {
+    return {
         headers,
         data
     }
 
-    return state
+}
+
+const updateSheetState = async (url) => {
+    const savedText = getState(url)
+    const text = await fetch(url).then(response => response.text())
+    if (savedText === text) return null
+    console.warn("Sheet state has changed")
+    setState(url, text)
+    return parseSheet(text)
 }
 
 
@@ -50,12 +66,13 @@ const updateMessage = (messages, idx = chooseRandomIdx(messages)) =>  {
     // message.innerText = messages[idx]
 }
 
-
-
 onLoaded(async () => {
-    const state = await getCSVData(url)
+    const text = getState(url)
+    const state = text ? parseSheet(text) : await updateSheetState(url)
+    if (text) updateSheetState(url) // Always update sheet state in the background to add new messages
     const messages = state.data.map(row => row['Message'])
-    const idx = (localStorage.getItem('idx') || 0) % messages.length
+    const idx = (getState('idx') || 0 )% messages.length
     updateMessage(messages, idx)
+    setState('idx', idx)
     localStorage.setItem('idx', idx + 1)
 })
